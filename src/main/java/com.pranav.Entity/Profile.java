8397cf74fa56886d724219afd39b801405eb7018
Pranav Gaira <148996644+pranav045@users.noreply.github.com>
2026-04-21 11:28:59 +0530

    Add Profile entity with user relationship

--- src/main/java/com.pranav.Entity/Profile.java
+++ src/main/java/com.pranav.Entity/Profile.java
@@ -1,1 +1,47 @@
+package com.pranav.Entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.OneToOne;
+
+@Entity
+public class Profile {
+	private String address;
+	private String email;
+	private String phone;
+	@OneToOne(mappedBy = "profile")
+	private User user;
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+}
 
