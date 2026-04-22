ea8babd2c8096ed8179ddad81511c16b28289514
Pranav Gaira <148996644+pranav045@users.noreply.github.com>
2026-04-22 21:03:37 +0530

    Add Book entity with id, name, and status fields

--- src/main/java/com.pranav.Entity/Book.java
+++ src/main/java/com.pranav.Entity/Book.java
@@ -1,1 +1,38 @@
+package com.pranav.Entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class Book {
+	@Id
+	private int id;
+	private String name;
+	private String status;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+}
+
 
