9aaf167903b99946312e542f61a089b33777f07d
Pranav Gaira <148996644+pranav045@users.noreply.github.com>
2026-04-22 21:05:34 +0530

    Add Category entity with getter and setter

--- src/main/java/com.pranav.Entity/Category.java
+++ src/main/java/com.pranav.Entity/Category.java
@@ -1,1 +1,18 @@
+package com.pranav.Entity;
+
+import jakarta.persistence.Entity;
+
+@Entity
+public class Category {
+	private String category;
+
+	public String getCategory() {
+		return category;
+	}
+
+	public void setCategory(String category) {
+		this.category = category;
+	}
+
+}
 
