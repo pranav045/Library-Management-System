f888927bb2eb7acf1a4df5131992f25433b8b97e
Pranav Gaira <148996644+pranav045@users.noreply.github.com>
2026-04-23 12:18:28 +0530

    Update Author.java

--- src/main/java/com.pranav.Entity/Author.java
+++ src/main/java/com.pranav.Entity/Author.java
@@ -1,1 +1,43 @@
 
+package com.pranav.Entity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+
+@Entity
+public class Author {
+	@Id
+	private int id;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public List<Book> getBooks() {
+		return books;
+	}
+
+	public void setBooks(List<Book> books) {
+		this.books = books;
+	}
+
+	private String name;
+	@OneToMany(mappedBy = "author")
+	private List<Book> books = new ArrayList<Book>();
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+}
