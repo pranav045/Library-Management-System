a0e40f212fa855495fa0f90f3b56614b03013c86
Pranav Gaira <148996644+pranav045@users.noreply.github.com>
2026-04-24 09:24:26 +0530

    Refactor Category class to use 'name' instead of 'category'

--- src/main/java/com.pranav.Entity/Category.java
+++ src/main/java/com.pranav.Entity/Category.java
@@ -1,18 +1,34 @@
 package com.pranav.Entity;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 
 @Entity
 public class Category {
-	private String category;
+	@Id
+	private String name;
 
-	public String getCategory() {
-		return category;
+	public List<Book> getBooks() {
+		return books;
 	}
 
-	public void setCategory(String category) {
-		this.category = category;
+	public void setBooks(List<Book> books) {
+		this.books = books;
+	}
+
+	@OneToMany(mappedBy = "category")
+	private List<Book> books = new ArrayList<>();
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
 	}
 
 }
-
