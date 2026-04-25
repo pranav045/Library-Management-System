30c1670ee8dc4dfc2d888a474dcf8baa68729a9c
Pranav Gaira <148996644+pranav045@users.noreply.github.com>
2026-04-25 14:36:13 +0530

    Create BookRepository interface for Book entity

--- src/main/java/com.pranav.Repository/BookRepository.java
+++ src/main/java/com.pranav.Repository/BookRepository.java
@@ -1,1 +1,12 @@
+package com.pranav.Repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.pranav.Entity.Book;
+
+@Repository
+public interface BookRepository extends JpaRepository<Book, Integer> {
+
+}
 
