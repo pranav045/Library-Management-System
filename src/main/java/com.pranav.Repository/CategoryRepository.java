af471c8821f852f40a179477cb158b63dd5f3061
Pranav Gaira <148996644+pranav045@users.noreply.github.com>
2026-04-25 14:37:24 +0530

    Add CategoryRepository interface for JPA

---
+++ src/main/java/com.pranav.Repository/CategoryRepository.java
@@ -0,0 +1,9 @@
+package com.pranav.Repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface Category extends JpaRepository<Category, Integer> {
+
+}
