a03eeeed9d2b81028a01a76a19193425df26acce
Pranav Gaira <148996644+pranav045@users.noreply.github.com>
2026-04-27 08:48:13 +0530

    Add UserDAO for user management operations

---
+++ src/main/java/com.pranav.DAO/userDAO.java
@@ -0,0 +1,30 @@
+package com.pranav.DAO;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.pranav.Entity.User;
+import com.pranav.Repository.UserRepository;
+
+@Repository
+public class UserDAO {
+	@Autowired
+	private UserRepository userRepository;
+
+	public User saveUser(User user) {
+		return userRepository.save(user);
+	}
+
+	public User findById(int id) {
+		return userRepository.findById(id).orElse(null);
+	}
+
+	public boolean deleteUser(int id) {
+		if (userRepository.existsById(id)) {
+			userRepository.deleteById(id);
+			return true;
+		}
+		return false;
+	}
+
+}
