cfcf2973fc5eea23f9ce6e956e56086d9a617524
Pranav Gaira <148996644+pranav045@users.noreply.github.com>
2026-05-02 14:51:56 +0530

    Add AuthorController for author management

---
+++ src/main/java/com.pranav.Controller/AuthorController.java
@@ -0,0 +1,29 @@
+package com.pranav.Controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.pranav.DTO.ResponseStructure;
+import com.pranav.Entity.Author;
+import com.pranav.Service.AuthorService;
+
+@RestController
+public class AuthorController {
+	@Autowired
+	private AuthorService authorService;
+
+	@PostMapping("/saveAuthor")
+	public ResponseEntity<ResponseStructure<Author>> saveAuthor(@RequestBody Author author) {
+		return authorService.saveAuthor(author);
+	}
+
+	@GetMapping("/getAuthor/{id}")
+	public ResponseEntity<ResponseStructure<Author>> getAppointment(@PathVariable int id) {
+		return authorService.getAuthor(id);
+	}
+}
