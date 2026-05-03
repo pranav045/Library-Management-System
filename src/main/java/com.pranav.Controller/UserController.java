95e766d53e8818a8790d978f3fe5e6b8a5aa1fc6
Pranav Gaira <148996644+pranav045@users.noreply.github.com>
2026-05-03 18:11:33 +0530

    Add UserController for user management operations

---
+++ src/main/java/com.pranav.Controller/UserController.java
@@ -0,0 +1,29 @@
+package com.pranav.Controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.pranav.DTO.ResponseStructure;
+import com.pranav.Entity.User;
+import com.pranav.Service.UserService;
+
+@RestController
+public class UserController {
+	@Autowired
+	private UserService userService;
+
+	@PostMapping("/saveUser")
+	public ResponseEntity<ResponseStructure<User>> saveUser(@RequestBody User user) {
+		return userService.saveUser(user);
+	}
+
+	@GetMapping("/getUser/{id}")
+	public ResponseEntity<ResponseStructure<User>> getUser(@PathVariable int id) {
+		return userService.getUser(id);
+	}
+}
