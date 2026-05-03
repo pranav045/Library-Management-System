67afb79b6037bf4602259d582463edd4c9970292
Pranav Gaira <148996644+pranav045@users.noreply.github.com>
2026-05-03 18:34:33 +0530

    Fix messages and handle user not found exception

--- src/main/java/com.pranav.Service/UserService.java
+++ src/main/java/com.pranav.Service/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 import com.pranav.DAO.UserDAO;
 import com.pranav.DTO.ResponseStructure;
 import com.pranav.Entity.User;
+import com.pranav.Exception.IdDoesNotPresentException;
 
 @Service
 public class UserService {
@@ -18,17 +19,21 @@ public class UserService {
 		User data = userDAO.saveUser(user);
 		ResponseStructure<User> rs = new ResponseStructure<User>();
 		rs.setData(data);
-		rs.setMessage("User Added Successfully");
+		rs.setMessage("Book Added Successfully");
 		rs.setStatusCode(HttpStatus.CREATED.value());
 		return new ResponseEntity<ResponseStructure<User>>(rs, HttpStatus.CREATED);
 	}
 
 	public ResponseEntity<ResponseStructure<User>> getUser(int id) {
 		User data = userDAO.getUser(id);
-		ResponseStructure<User> rs = new ResponseStructure<User>();
-		rs.setData(data);
-		rs.setMessage("User Found Successfully");
-		rs.setStatusCode(HttpStatus.FOUND.value());
-		return new ResponseEntity<ResponseStructure<User>>(rs, HttpStatus.FOUND);
+		if (data != null) {
+			ResponseStructure<User> rs = new ResponseStructure<User>();
+			rs.setData(data);
+			rs.setMessage("Book Found Successfully");
+			rs.setStatusCode(HttpStatus.FOUND.value());
+			return new ResponseEntity<ResponseStructure<User>>(rs, HttpStatus.FOUND);
+		} else {
+			throw new IdDoesNotPresentException("Id" + id + " doesA not found");
+		}
 	}
 }
