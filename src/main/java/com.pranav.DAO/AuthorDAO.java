1174b4ea57ab49022d4811d1ebaef077527dfdd7
Pranav Gaira <148996644+pranav045@users.noreply.github.com>
2026-05-09 15:23:09 +0530

    Add method to find all authors in AuthorDAO

--- src/main/java/com.pranav.DAO/AuthorDAO.java
+++ src/main/java/com.pranav.DAO/AuthorDAO.java
@@ -1,5 +1,7 @@
 package com.pranav.DAO;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
@@ -17,6 +19,10 @@ public class AuthorDAO {
 
 	public Author findAuthor(int id) {
 		return authorRepository.findById(id).orElse(null);
+	}
+
+	public List<Author> findAllAuthor() {
+		return authorRepository.findAll();
 	}
 
 	public boolean deleteAuthor(int id) {
