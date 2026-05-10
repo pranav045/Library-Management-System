35fc29e840c627b035f36d53c11e09e6915f0dca
Pranav Gaira <148996644+pranav045@users.noreply.github.com>
2026-05-10 12:21:19 +0530

    Refactor User class for annotations and method order
    Refactored User class to fix annotations and reorganize methods.

--- src/main/java/com.pranav.Entity/User.java
+++ src/main/java/com.pranav.Entity/User.java
@@ -7,32 +7,22 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
 
 @Entity
+@Table(name = "users")
 public class User {
+
 	@Id
 	private int id;
+
 	private String name;
-	@OneToOne(mappedBy = "user")
+
+	@OneToOne(mappedBy = "user") // fix here
 	private Profile profile;
+
 	@OneToMany(mappedBy = "user")
-	private List<Book> books = new ArrayList<Book>();
-
-	public List<Book> getBooks() {
-		return books;
-	}
-
-	public void setBooks(List<Book> books) {
-		this.books = books;
-	}
-
-	public Profile getProfile() {
-		return profile;
-	}
-
-	public void setProfile(Profile profile) {
-		this.profile = profile;
-	}
+	private List<Book> books = new ArrayList<>();
 
 	public int getId() {
 		return id;
@@ -48,5 +38,21 @@ public class User {
 
 	public void setName(String name) {
 		this.name = name;
+	}
+
+	public Profile getProfile() {
+		return profile;
+	}
+
+	public void setProfile(Profile profile) {
+		this.profile = profile;
+	}
+
+	public List<Book> getBooks() {
+		return books;
+	}
+
+	public void setBooks(List<Book> books) {
+		this.books = books;
 	}
 }
