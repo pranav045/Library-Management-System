07acb2111174465846303c6003cf67847b681707
Pranav Gaira <148996644+pranav045@users.noreply.github.com>
2026-05-11 08:40:55 +0530

    Update CategoryRepository.java

--- src/main/java/com.pranav.Repository/CategoryRepository.java
+++ src/main/java/com.pranav.Repository/CategoryRepository.java
@@ -3,7 +3,9 @@ package com.pranav.Repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.pranav.Entity.Category;
+
 @Repository
-public interface Category extends JpaRepository<Category, Integer> {
+public interface CategoryRepository extends JpaRepository<Category, String> {
 
 }
