71de68d5821f3cfc92f5686ffc23fd3996a6d728
Pranav Gaira <148996644+pranav045@users.noreply.github.com>
2026-05-12 09:17:51 +0530

    Add deleteUser endpoint to UserController

--- src/main/java/com.pranav.Controller/UserController.java
+++ src/main/java/com.pranav.Controller/UserController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,6 +17,7 @@ import com.pranav.Service.UserService;
 
 @RestController
 public class UserController {
+
 	@Autowired
 	private UserService userService;
 
@@ -32,5 +34,10 @@ public class UserController {
 	@GetMapping("/getAllUsers")
 	public ResponseEntity<ResponseStructure<List<User>>> getAllUsers() {
 		return userService.getAllUsers();
+	}
+
+	@DeleteMapping("/deleteUser/{id}")
+	public ResponseEntity<ResponseStructure<User>> deleteUser(@PathVariable int id) {
+		return userService.deleteUser(id);
 	}
 }
